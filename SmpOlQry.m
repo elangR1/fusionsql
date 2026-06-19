b9c2ed9a18f44ea754e3fedc7d3a831dc83712cc
elgR1 <32348595+elangR1@users.noreply.github.com>
2026-06-19 09:45:04 +0700

    Update SmpOlQry.m

--- SmpOlQry.m
+++ SmpOlQry.m
@@ -4,8 +4,8 @@ let
     SqlText = Text.FromBinary(SqlBinary),
 
     // 2. Mandatory Parameter Logic
-    StartText = DateTime.ToText(RangeStart, "yyyy-MM-dd HH:mm:ss"),
-    EndText = DateTime.ToText(RangeEnd, "yyyy-MM-dd HH:mm:ss"),
+    StartDt = DateTime.ToText(RangeStart, "yyyy-MM-dd HH:mm:ss"),
+    EndDt = DateTime.ToText(RangeEnd, "yyyy-MM-dd HH:mm:ss"),
 
     // 3. Conditional Logic: Optional dynamic filters
     F_ItemLike = if ParamItemNumberLike <> null and ParamItemNumberLike <> "" then " AND esi.ITEM_NUMBER LIKE '" & ParamItemNumberLike & "'" else "",
@@ -14,10 +14,10 @@ let
     F_DescNot  = if ParamDescriptionNotLike <> null and ParamDescriptionNotLike <> "" then " AND esi.description NOT LIKE '" & ParamDescriptionNotLike & "'" else "",
     F_OrgIn    = if ParamOrgNameIn <> null and ParamOrgNameIn <> "" then " AND iodv.ORGANIZATION_NAME IN (" & ParamOrgNameIn & ")" else "",
     F_Org      = if ParamOrgName <> null and ParamOrgName <> "" then " AND iodv.ORGANIZATION_NAME = '" & ParamOrgName & "'" else "",
-    ActiveFilters = F_ItemLike & F_Class & F_DescLike & F_DescNot & F_OrgIn & F_Org,
+    CondFilters = F_ItemLike & F_Class & F_DescLike & F_DescNot & F_OrgIn & F_Org,
 
-    InjectDates = Text.Replace(Text.Replace(SqlText, "__START_DATE__", StartText), "__END_DATE__", EndText),
-    FinalSQL    = Text.Replace(InjectDates, "__DYNAMIC_FILTERS__", ActiveFilters),
+    InjectDates = Text.Replace(Text.Replace(SqlText, "__START_DATE__", StartDt), "__END_DATE__", EndDt),
+    FinalSQL    = Text.Replace(InjectDates, "__DYNAMIC_FILTERS__", CondFilters),
 
     ProxyUrl = "https://stringfellow-fsn-pstgrs-prxy.hf.space/query",
     JsonPayload = Binary.Buffer(Json.FromValue([sql = FinalSQL])),

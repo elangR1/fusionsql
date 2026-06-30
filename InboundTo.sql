61c8539891d743cc4851a0910ed1c298bac00f48
elgR1 <32348595+elangR1@users.noreply.github.com>
2026-06-30 10:01:07 +0700

    Update InboundTo.sql

--- InboundTo.sql
+++ InboundTo.sql
@@ -103,8 +103,9 @@ LEFT JOIN
     AND itol.inventory_item_id = wdd.inventory_item_id
 WHERE
   -- itoh.ordered_date BETWEEN '__START_DATE__' AND '__END_DATE__'
+  --  itol.creation_date BETWEEN '__START_DATE__' AND '__END_DATE__
   -- itoh.ordered_date BETWEEN TO_DATE('__START_DATE__', 'YYYY-MM-DD') AND TO_DATE('__END_DATE__', 'YYYY-MM-DD') + 1
-  itol.creation_date BETWEEN '__START_DATE__' AND '__END_DATE__
+  itol.creation_date BETWEEN TO_DATE('__START_DATE__', 'YYYY-MM-DD') AND TO_DATE('__END_DATE__', 'YYYY-MM-DD') + 1
 ORDER BY
 wdd.source_line_id ASC
 , iodv_source.organization_name
